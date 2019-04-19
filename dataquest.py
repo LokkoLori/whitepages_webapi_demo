2e00e61be2c0a495900f4e1dd1d6f4d2e5d25578
Kaptás András <andras.kaptas@nng.com>
2019-04-19 13:46:08 +0200

    write result into a file, add some comments

--- dataquest.py
+++ dataquest.py
@@ -5,7 +5,6 @@ import urllib.parse
 import json
 import usaddress
 
-
 # hard coded api keys for first experiments
 api_key_reverse_phone = '7f365d38fb47449a985ccc972e7818aa'
 api_key_find_person = '44c5b314db5b4dd0a9a7c2454fbbdd02'
@@ -39,6 +38,9 @@ def extractaddress(wp_addr_dict):
     raddrd = [ad for ad in raddrd if ad and ad != "None"]
     return ", ".join(raddrd)
 
+'''
+represent a contact, it has 3 fileds: name, address, phone
+'''
 class Contact():
 
     all_contacts = []
@@ -53,6 +55,8 @@ class Contact():
         self.get_data_from_whitepages()
 
     def get_data_from_whitepages(self):
+        # this is the main processing method, push it into thread a pool for parallel processing...
+
 
         # fill data by phone number
         if self.phone:
@@ -64,6 +68,7 @@ class Contact():
                 rdata = json.loads(response.read())
 
                 if rdata.get("belongs_to"):
+                    #todo: check if it similar to the original data ... but what if it is not?
                     self.name = rdata["belongs_to"][0].get("name")
                 if rdata.get("current_addresses"):
                     self.address = extractaddress(rdata["current_addresses"][0])
@@ -114,12 +119,13 @@ class Contact():
                 self.address = extractaddress(person["found_at_address"])
 
 
-
+'''
+a class reprezent the csv data
+'''
 class CSV_form():
 
-
     def __init__(self, field_list):
-        self.filed_list = field_list
+        self.field_list = field_list
         self.contact_filed_idx = []
         expected_fields = ("name", "address", "phone")
 
@@ -143,17 +149,42 @@ class CSV_form():
 
     def addRow(self, row):
 
-        self.data.append((list(row),
+        self.data.append((row,
                           Contact(
                               name=row[self.contact_filed_idx[0]],
                               address=row[self.contact_filed_idx[1]],
                               phone=row[self.contact_filed_idx[2]])
                          ))
 
+    def write_to(self, f):
+        '''
+        write the current data into a stream in cvs txt
+        :param f: iostream
+        :return:
+        '''
+        f.write(",".join(['"'+f+'"' for f in self.field_list])+"\n")
 
-def process_csv(f):
+        for data in self.data:
 
-    reader = csv.reader(f, delimiter=',')
+            row = list(data[0])
+
+            contact = data[1]
+            row[self.contact_filed_idx[0]] = contact.name
+            row[self.contact_filed_idx[1]] = contact.address
+            row[self.contact_filed_idx[2]] = contact.phone
+
+            f.write(",".join(['"' + d + '"' for d in row]) + "\n")
+
+
+def process_csv(input, output):
+    '''
+    accept a csv text from the input then write the filled csv data on the output ... these could be files id StringIO
+    :param input: iostream
+    :param output: iostream
+    :return:
+    '''
+
+    reader = csv.reader(input, delimiter=',')
 
     first_row = True
     for row in reader:
@@ -163,14 +194,11 @@ def process_csv(f):
         else:
             csv_form.addRow(row)
 
-    pass
+    csv_form.write_to(output)
 
-
-def process_csv_text(txt):
-
-    StringIO(txt)
 
 if __name__ == "__main__":
 
     with open("sample.txt", encoding="utf-8") as f:
-        process_csv(f)
+        with open("result.csv", "w", encoding="utf-8") as r:
+            process_csv(f, r)
